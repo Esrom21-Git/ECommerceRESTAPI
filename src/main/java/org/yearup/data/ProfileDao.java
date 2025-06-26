ce65f9d854d4ad2a53225a2ff0af665db5f35a14
Esrom Tekle <kidaneesrom34@gmail.com>
2025-06-26 14:58:12 -0700

    Complted the My Profile

--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -3,7 +3,13 @@ package org.yearup.data;
 
 import org.yearup.models.Profile;
 
+import javax.validation.Valid;
+
 public interface ProfileDao
 {
     Profile create(Profile profile);
+
+    Profile getByUserId(int id);
+
+    Profile update(int id, @Valid Profile profile);
 }
