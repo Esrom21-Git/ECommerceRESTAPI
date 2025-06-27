c6c915bc8cc424261b3856fe30f5d98c03028999
Esrom Tekle <kidaneesrom34@gmail.com>
2025-06-26 21:36:58 -0700

    made it work with the shopping cart

--- src/main/java/org/yearup/data/SimpleShoppingCartDao.java
+++ src/main/java/org/yearup/data/SimpleShoppingCartDao.java
@@ -1,74 +1,72 @@
-//// ShoppingCartDao.java
-//package org.yearup.data;
-//
-//import org.yearup.models.ShoppingCart;
-//
-//interface ShoppingCartDao {
-//    ShoppingCart getByUserId(int userId);
-//    void addItem(int userId, int productId, int quantity);
-//    void updateItem(int userId, int productId, int quantity);
-//    void removeItem(int userId, int productId);
-//    void clearCart(int userId);
-//}
-//
-//// SimpleShoppingCartDao.java
-//package org.yearup.data;
-//
-//import org.yearup.models.ShoppingCart;
-//import org.yearup.models.ShoppingCartItem;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class SimpleShoppingCartDao implements ShoppingCartDao {
-//
-//    private Map<Integer, ShoppingCart> carts = new HashMap<>();
-//
-//    @Override
-//    public ShoppingCart getByUserId(int userId) {
-//        if (carts.get(userId) == null) {
-//            carts.put(userId, new ShoppingCart());
-//        }
-//        return carts.get(userId);
-//    }
-//
-//    @Override
-//    public void addItem(int userId, int productId, int quantity) {
-//        ShoppingCart cart = getByUserId(userId);
-//        ShoppingCartItem item = cart.get(productId);
-//
-//        if (item != null) {
-//            item.setQuantity(item.getQuantity() + quantity);
-//        } else {
-//            ShoppingCartItem newItem = new ShoppingCartItem();
-//            newItem.setProductId(productId);
-//            newItem.setQuantity(quantity);
-//            cart.add(newItem);
-//        }
-//    }
-//
-//    @Override
-//    public void updateItem(int userId, int productId, int quantity) {
-//        ShoppingCart cart = getByUserId(userId);
-//        ShoppingCartItem item = cart.get(productId);
-//
-//        if (item != null) {
-//            if (quantity <= 0) {
-//                cart.remove(productId);
-//            } else {
-//                item.setQuantity(quantity);
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void removeItem(int userId, int productId) {
-//        ShoppingCart cart = getByUserId(userId);
-//        cart.remove(productId);
-//    }
-//
-//    @Override
-//    public void clearCart(int userId) {
-//        ShoppingCart cart = getByUserId(userId);
-//        cart.clear();
-//    }
-//}
+package org.yearup.data;
+
+import org.yearup.models.ShoppingCartItem;
+import java.util.List;
+
+public interface SimpleShoppingCartDao {
+    /**
+     * Retrieves all items in the shopping cart for a specific user
+     * @param userId the ID of the user
+     * @return List of ShoppingCartItem objects
+     */
+    List<ShoppingCartItem> getCartByUserId(int userId);
+
+    /**
+     * Adds a product to the user's shopping cart with default quantity of 1
+     * @param userId the ID of the user
+     * @param productId the ID of the product to add
+     */
+    void addProductToCart(int userId, int productId);
+
+    /**
+     * Adds a product to the user's shopping cart with specified quantity
+     * @param userId the ID of the user
+     * @param productId the ID of the product to add
+     * @param quantity the quantity of the product to add
+     */
+    void addProductToCart(int userId, int productId, int quantity);
+
+    /**
+     * Updates the quantity of a specific product in the user's cart
+     * @param userId the ID of the user
+     * @param productId the ID of the product to update
+     * @param quantity the new quantity
+     */
+    void updateProductQuantity(int userId, int productId, int quantity);
+
+    /**
+     * Removes a specific product from the user's cart
+     * @param userId the ID of the user
+     * @param productId the ID of the product to remove
+     */
+    void removeProductFromCart(int userId, int productId);
+
+    /**
+     * Clears all items from the user's shopping cart
+     * @param userId the ID of the user
+     */
+    void clearCart(int userId);
+
+    /**
+     * Gets the total number of items in the user's cart
+     * @param userId the ID of the user
+     * @return total quantity of all items
+     */
+    int getCartItemCount(int userId);
+
+    /**
+     * Checks if a specific product exists in the user's cart
+     * @param userId the ID of the user
+     * @param productId the ID of the product to check
+     * @return true if product exists in cart, false otherwise
+     */
+    boolean isProductInCart(int userId, int productId);
+
+    /**
+     * Gets a specific cart item by user ID and product ID
+     * @param userId the ID of the user
+     * @param productId the ID of the product
+     * @return ShoppingCartItem if found, null otherwise
+     */
+    ShoppingCartItem getCartItem(int userId, int productId);
+}
